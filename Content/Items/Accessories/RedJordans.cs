2b435f4d1c8a01ff096ef4343529adb5c1ad11bc
Gerald Hill <gh.blockt9@gmail.com>
2024-12-06 16:59:02 -0800

    Initial Commit

---
+++ Content/Items/Accessories/RedJordans.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BlocktsVanities.Content.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Shoes)]
+    public class RedJordans : ModItem
+    {
+        public override void SetDefaults() {
+            Item.width = 22;
+            Item.height = 22;
+
+            Item.accessory = true;
+            Item.vanity = true;
+            Item.rare = ItemRarityID.Orange;
+            Item.value = Item.buyPrice(0, 5, 0, 0);
+        }
+    }
+}
