97af98bec1f72338e351863a384c94257bf2c57e
Gerald Hill <gh.blockt9@gmail.com>
2024-12-06 20:53:59 -0800

    Implemented Jordans
    Jordans can be bought from Clothier

---
+++ Content/NPCs/BlocktGlobalTownNPC.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+using BlocktsVanities.Content.Items.Accessories;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace BlocktsVanities.Content.NPCs
+{
+    internal class BlocktGlobalTownNPC : GlobalNPC
+    {
+        public override void ModifyShop(NPCShop shop)
+        {
+            int type = shop.NpcType;
+
+            if (type == NPCID.Clothier)
+            {
+                shop.Add(ModContent.ItemType<RedJordans>());
+            }
+        }
+    }
+}
